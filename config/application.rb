b804c65c4ca436fb2b12aba7d46a884e6f8d1cd1
pgbuker <kirill.bezik@gmail.com>
2022-12-01 15:03:22 +0300

    feat: add version

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module TestGuru
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    config.i18n.default_locale = :ru
+    config.time_zone = "Moscow"
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
