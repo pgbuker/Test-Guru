cacad46cb559f3b58a781ca09878420e38cfb43e
pgbuker <kirill.bezik@gmail.com>
2022-12-04 16:25:25 +0300

    add static pages in folder public

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'about/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/about', to: 'about#index'
-  get '/about/author'
-
 end
