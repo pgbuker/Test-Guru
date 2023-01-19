9d66b28ce8d47124f9c5e51073a857abea7fadc8
pgbuker <kirill.bezik@gmail.com>
2023-01-19 21:22:48 +0300

    feat: add method to sort tests by category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
+  def self.sort_categories(category)
+    joins('JOIN categories ON categories.id = tests.category_id').where(categories: { title: category }).order('tests.title DESC').pluck(:title)
+  end
 end
