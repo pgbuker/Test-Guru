1954c83c33b08991f42a487808582fc1a115b265
pgbuker <kirill.bezik@gmail.com>
2023-01-30 11:33:18 +0300

    fix: add relations between User and Test for author

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   
   has_many :passed_tests
   has_many :tests, through: :passed_tests
+  has_many :author, class_name: 'Test', foreign_key: :author_id
 
   def passed_tests(level)
     tests.where(level: level)
