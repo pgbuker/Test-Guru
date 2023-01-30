eaed90ac626f5f3936ebf1577ea7d15578fac59a
pgbuker <kirill.bezik@gmail.com>
2023-01-30 21:47:24 +0300

    changed name association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   
   has_many :passed_tests
   has_many :tests, through: :passed_tests
-  has_many :author, class_name: 'Test', foreign_key: :author_id
+  has_many :author_tests, class_name: 'Test', foreign_key: :author_id
 
   def passed_tests(level)
     tests.where(level: level)
