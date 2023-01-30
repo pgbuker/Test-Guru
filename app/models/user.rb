7f64f2d9e795497e4635aeedb657d6a33579e798
pgbuker <kirill.bezik@gmail.com>
2023-01-30 10:43:10 +0300

    refactored methods for Test and User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
-  # has_and_belongs_to_many :tests
+  
   has_many :passed_tests
   has_many :tests, through: :passed_tests
 
   def passed_tests(level)
-    Test.joins('JOIN passed_tests ON tests.id = passed_tests.test_id').where(
-      'passed_tests.user_id = ? AND tests.level = ?', id, level
-    )
+    tests.where(level: level)
   end
 end
