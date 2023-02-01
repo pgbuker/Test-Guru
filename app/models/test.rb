8d139a5a7a725ffdd7431fb32005387da2b89c35
pgbuker <kirill.bezik@gmail.com>
2023-02-01 22:48:23 +0300

    refactoring scope in test, added migration and unique index to tests table

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,21 +5,22 @@ class Test < ApplicationRecord
   has_many :passed_tests
   has_many :users, through: :passed_tests
 
-  scope :easy_level, -> { where(level: 0..1).order(title: :asc) }
-  scope :medium_level, -> { where(level: 2..4).order(title: :asc) }
-  scope :hard_level, -> { where(level: 5..Float::INFINITY).order(title: :asc) }
+  scope :easy_level, -> { where(level: 0..1) }
+  scope :medium_level, -> { where(level: 2..4) }
+  scope :hard_level, -> { where(level: 5..Float::INFINITY) }
 
-  scope :sort_by_categories, lambda { |category|
-                               joins(:category).where(categories: { title: category }).order('tests.title DESC').pluck(:title)
+  scope :view_categories, lambda { |category|
+                               joins(:category).where(categories: { title: category })
                              }
-  scope :sort_by_level, ->(level) { where(level: level) }
+  scope :view_level, ->(level) { where(level: level) }
 
   validates :title, presence: true,
                     uniqueness: { scope: :level, message: 'Может быть только один тест с таким названием и уровнем!' }
 
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def self.sort_categories(category)
-    joins(:category).where(categories: { title: category }).order('tests.title DESC').pluck(:title)
+  def self.sort_by_categories(category)
+    view_categories(category).order('tests.title DESC').pluck(:title)
   end
+
 end
