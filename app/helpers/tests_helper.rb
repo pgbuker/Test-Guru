3e30034ea8552285793321cf78d5fdd809631080
pgbuker <kirill.bezik@gmail.com>
2023-02-17 12:21:11 +0300

    video lesson code for tests

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,22 @@
 module TestsHelper
+
+  TEST_LEVEL = { 0 => :easy, 1 => :elementary, 2 => :advanced, 3 => :hard }.freeze
+
+  def test_level(test)
+    TEST_LEVEL[test.level] || :hero
+  
+    # case test.level 
+    # when 0  
+    #   'easy'
+    # when 1
+    #   'elementary'
+    # when 2  
+    #   'advanced'
+    # when 3
+    #   'hard'
+    # else
+    #   'hero'
+    # end
+  end
+
 end
