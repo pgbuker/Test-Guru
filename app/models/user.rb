3038045dbf1fad44936e094f393c4e2f6584743a
pgbuker <kirill.bezik@gmail.com>
2023-03-02 01:57:16 +0300

    add Authentification

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
+  
+  include Auth
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
 
-  validates :name, :surname, :email, presence: true
+  # has_secure_password
 
+  validates :name, :surname, :email, presence: true
+ 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
   end
@@ -12,4 +19,5 @@ class User < ApplicationRecord
   def test_by_level(level)
     tests.where(level: level)
   end
+
 end
