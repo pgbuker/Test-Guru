1e883a63bc451ceb18969c8e6d662f7bdfffd9fc
pgbuker <kirill.bezik@gmail.com>
2023-03-01 14:40:49 +0300

    refactor: removing the callback set_user

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,6 @@
 class TestsController < ApplicationController
   before_action :find_test, only: %i[show edit update destroy start]
-  before_action :set_user, only: %i[start]
-
+ 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
@@ -41,6 +40,7 @@ class TestsController < ApplicationController
   end
 
   def start
+    @user = User.first
     @user.tests.push(@test)
     redirect_to @user.test_passage(@test)
   end
@@ -53,10 +53,6 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def set_user
-    @user = User.first
   end
 
   def rescue_with_test_not_found
