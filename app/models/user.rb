762df59169480a241f8a107c6632149f4d77c4e7
pgbuker <kirill.bezik@gmail.com>
2023-03-02 02:01:04 +0300

    add has_secure_password, delete Auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,11 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
   
-  include Auth
-
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
 
-  # has_secure_password
+  has_secure_password
 
   validates :name, :surname, :email, presence: true
  
