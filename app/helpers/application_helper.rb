1f5c410aa558c6838282441eb5b02d62f7ce73ee
pgbuker <kirill.bezik@gmail.com>
2023-03-03 20:08:45 +0300

    create helper for flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to 'Проект', "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def flash_helper(key)
+    content_tag :p, flash[key], class: "flash #{key}" if flash[key]
+  end
+  
 end
+ 
+ 
