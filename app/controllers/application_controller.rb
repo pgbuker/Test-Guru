681097c78360e8f1203ca3f3741c86322bb25114
pgbuker <kirill.bezik@gmail.com>
2023-03-09 00:33:05 +0300

    add redirecting admins to the /admin/tests page on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 
+
   before_action :configure_sign_up_params, if: :devise_controller?
   before_action :authenticate_user!
   
@@ -10,4 +11,13 @@ class ApplicationController < ActionController::Base
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :surname])
   end
+
+  def after_sign_in_path_for(current_user)
+    if current_user.is_a?(Admin)
+      admin_tests_path
+    else
+      root_path
+    end
+  end
+
 end
