55fb877dfdbc618667abcbf367b2fad7cd9f6f6c
pgbuker <kirill.bezik@gmail.com>
2023-03-10 00:32:07 +0300

    add localization, I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_sign_up_params, if: :devise_controller?
   before_action :authenticate_user!
+  before_action :set_locale
+
+  def default_url_options
+    { lang: I18n.locale }    
+  end
   
   
   
@@ -19,6 +24,10 @@ class ApplicationController < ActionController::Base
     else
       root_path
     end
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 
 end
