dfceed9d2c16d24ea6ecc4e39e53b3a03ba60316
pgbuker <kirill.bezik@gmail.com>
2023-04-10 18:35:31 +0300

    add view gists for admin

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -20,7 +20,8 @@ class TestPassagesController < ApplicationController
   def gist
     result = GistQuestionService.new(@test_passage.current_question).call
     
-  if result.success?
+  if result.html_url.present?
+    current_user.gists.create!(question: @test_passage.current_question, url: result.html_url)
     flash[:notice] = t('.success', url: result.html_url)
   else
     flash[:alert] = t('.failure', url: result.html_url)
