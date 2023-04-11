b78d30f3b3017c0a9646d2c9b9c2d9cff6ff29f0
pgbuker <kirill.bezik@gmail.com>
2023-04-11 11:00:56 +0300

    delete bang method

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -21,10 +21,10 @@ class TestPassagesController < ApplicationController
     result = GistQuestionService.new(@test_passage.current_question).call
     
   if result.html_url.present?
-    current_user.gists.create!(question: @test_passage.current_question, url: result.html_url)
+    current_user.gists.create(question: @test_passage.current_question, url: result.html_url)
     flash[:notice] = t('.success', url: result.html_url)
   else
-    flash[:alert] = t('.failure', url: result.html_url)
+    flash[:alert] = t('.failure')
   end
 
     redirect_to @test_passage
