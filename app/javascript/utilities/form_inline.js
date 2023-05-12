48d946a940b5dc36f86478c1604cae4b9cbee17a
pgbuker <kirill.bezik@gmail.com>
2023-05-12 18:28:27 +0300

    delete comments

--- app/javascript/utilities/form_inline.js
+++ app/javascript/utilities/form_inline.js
@@ -1,11 +1,6 @@
 document.addEventListener('turbolinks:load', function() {
   var controls = document.querySelectorAll('.form-inline-link')
 
-  // if(controls.length) {
-  //   for (var i = 0; i < controls.length; i++) {
-  //     controls[i].addEventListener('click', formInlineLinkHandler)
-  //   }
-  // }
   
   $('.form-inline-link').on('click', formInlineLinkHandler)
 
@@ -26,8 +21,7 @@ function formInlineLinkHandler(event) {
 
 function formInlineHandler(testId) {
   var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
-  // var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
-  // var formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
+
 
   var $testTitle = $('.test-title[data-test-id="'+ testId + '"]')
   var $formInline = $('.form-inline[data-test-id="'+ testId + '"]')
