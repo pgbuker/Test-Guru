31b2b16ec21c02fdb1675f9ce8efc1036240f97f
pgbuker <kirill.bezik@gmail.com>
2023-06-09 14:25:39 +0300

    add message if user choose next question witout answer

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -7,6 +7,7 @@ class TestPassagesController < ApplicationController
   def result; end
 
   def update
+    return redirect_to test_passage_path(@test_passage), alert: t('.choose_without_answer') unless params[:answer_ids] 
     @test_passage.accept!(params[:answer_ids])
 
     if @test_passage.completed?
